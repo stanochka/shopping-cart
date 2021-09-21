ad2cc854985861b2a00385f666acc139595a0161
stanochka <krozzochka@gmail.com>
2021-09-21 13:50:26 +0300

    Styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 export const Cart = ({items}) => {
+    let sum = items.length ? items.map(i => i.price).reduce((a,b) => a+b) : 0;
+
     return (
         <div className='Cart'>
-            {items.map((item, i) => <div key={i}>{item.title}</div>)}
+            <h1>Shopping Cart</h1>
+            {items.map((item, i) => (
+              <div className='CartItem' key={i}>
+                <div className='CartItemTitle'><Link to={'/items/' + item.id}>{item.title}</Link></div>
+                <img className='CartItemImage' src={item.image} alt={item.title} />
+                <div className='CartPrice'>${item.price}</div>
+              </div>
+            ))}
+            <div className='CartTotal'>Total: ${sum}</div>
+            <button className='Payment' onClick={() => alert('Thanks for stopping by!')}>Proceed to Payment</button>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,8 @@
 export const Home = () => {
-    return <main>Home</main>
+    return (
+      <div className='Home'>
+        <h1>Fake Shop</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+    )
 }
